c9858656ce70c67ec779413f5aedfd9a537e963b
CodexVII <keitaro_ayanokoji@yahoo.ie>
2016-12-30 16:51:33 +0000

    Logout enabled with bean

---
+++ FirstApp/src/bean/AuthBackingBean.java
@@ -0,0 +1,24 @@
+package bean;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.RequestScoped;
+import javax.faces.context.FacesContext;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+
+@RequestScoped
+@ManagedBean
+public class AuthBackingBean {
+	public String logout(){
+		String result = "/index?faces-redirect=true";
+		FacesContext context = FacesContext.getCurrentInstance();
+		HttpServletRequest request = (HttpServletRequest)context.getExternalContext().getRequest();
+		
+		try{
+			request.logout();
+		}catch(ServletException e){
+			result = "/loginError?faces-redirect=true";
+		}
+		return result;
+	}
+}
