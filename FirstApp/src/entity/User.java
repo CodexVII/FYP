7a53fef370afb1f8a179870aec44f4ed908a9ff1
CodexVII <keitaro_ayanokoji@yahoo.ie>
2016-12-30 11:26:33 +0000

    connection to DB not available

---
+++ FirstApp/src/entity/User.java
@@ -0,0 +1,41 @@
+package entity;
+
+import java.io.Serializable;
+import javax.persistence.*;
+
+
+/**
+ * The persistent class for the t_user database table.
+ * 
+ */
+@Entity
+@Table(name="t_user")
+@NamedQuery(name="User.findAll", query="SELECT u FROM User u")
+public class User implements Serializable {
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	private int id;
+
+	private String user;
+
+	public User() {
+	}
+
+	public int getId() {
+		return this.id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getUser() {
+		return this.user;
+	}
+
+	public void setUser(String user) {
+		this.user = user;
+	}
+
+}
